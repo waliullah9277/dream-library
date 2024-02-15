35888f5e5c90c4f61b6e43fbf40bc7bcfa592c2f
Waliullah <waliullah9252@gmail.com>
2024-02-15 17:46:57 +0600

    media file directory changes

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -15,6 +15,7 @@ import dj_database_url
 import environ
 env = environ.Env()
 environ.Env.read_env()
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,12 +152,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR / "static",)
+# ]
 
 # Base url to serve media files
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
